f6c3703126cce77b4e85d85167bbe67ab9deceeb
amansgz <amansgz.ar@gmail.com>
2026-02-14 15:53:45 -0300

    style: modify class name to is-active for toggle functionality

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const btnShare = document.getElementById("btnShare");
 
 btnShare.addEventListener("click", () => {
   /* the css controls show / hide elements article author content and article nav, childs of article author */
-  articleAuthor.classList.toggle("active");
+  articleAuthor.classList.toggle("is-active");
 });
